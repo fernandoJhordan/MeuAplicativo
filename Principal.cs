10842a654a1f845900c35617b2f2ba83c8521ed1
Fernando Michael <nando_michael@hhotmail.com>
2024-10-17 16:06:22 -0300

    Aula30 Estruct

--- Principal.cs
+++ Principal.cs
@@ -12,17 +12,31 @@ namespace MeuAplicativo
 {
     public partial class Principal : Form
     {
-    
+        //globa estrutura
+        struct EstruturaPessoa 
+        {
+            public string Nome;
+            public string Sobre;
+        }
+
+
         public Principal()
         {
             InitializeComponent();
         }
 
         private void btnOK_Click(object sender, EventArgs e)
-        {
-            //ClasseNormal Cn = new ClasseNormal();
+        {   
+            //atribuindo valor as propriedades
+            EstruturaPessoa Ep = new EstruturaPessoa()
+            {
+                Nome = "Alex", Sobre="Rodrigues"
+            };
+            //atribuindo valor as propriedades
+            //Ep.Nome = "Alex";
+            //Ep.Sobre = "Rodrigues";
 
-            MessageBox.Show(ClasseEstatica.Nome);
+            MessageBox.Show("Sou " + Ep.Nome + " "  + Ep.Sobre);
         }
 
         private void Principal_Load(object sender, EventArgs e)
